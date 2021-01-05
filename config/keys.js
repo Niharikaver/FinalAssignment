5f99ecb8611488ec66952d260061ad2571179f50
Niharikaver <nimishaverma96@gmail.com>
2021-01-05 01:45:58 -0500

    final push

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  google: {
+    clientID:
+      "1092965417301-q795r84ec1l3b93ge3pub60acms6598u.apps.googleusercontent.com",
+    clientSecret: "H0dH4weKhgHymyNWctjwtgJf",
+  },
+  MongoURI:"mongodb+srv://niharika:niharika@cluster0.dwqnt.mongodb.net/niharika",
+  secret: "secretkry",
+};
